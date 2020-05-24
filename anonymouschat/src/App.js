71de3393911165ca3d0ed1cf754d033fb68e8d06
davidbehr <david.behr@stackworx.io>
2020-05-24 17:31:10 +0200

    finished off chat app

--- anonymouschat/src/App.js
+++ anonymouschat/src/App.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
 import { connect, sendMsg } from "./api";
 import { Header } from "./api/Header";
 import { ChatHistory } from "./api/ChatHistory";
 
 function App() {
   const [chatHistory, setChatHistory] = useState([]);
+  const [message, setMessage] = useState("");
   useEffect(() => {
     connect((msg) => {
       console.log("New Message:", msg);
@@ -17,13 +16,19 @@ function App() {
     });
   });
   function send() {
-    sendMsg("hello");
+    sendMsg(message);
+    console.log(message);
     console.log(chatHistory);
   }
   return (
     <div className="App">
       <Header />
       <ChatHistory chatHistory={chatHistory} />
+
+      <input
+        type="text"
+        onChange={(val) => setMessage(val.currentTarget.value)}
+      />
       <button onClick={() => send()}>Hit</button>
     </div>
   );
--- main.go
+++ main.go
@@ -8,17 +8,30 @@ import (
 	"github.com/davebehr1/anonymous-chat/pkg/websocket"
 )
 
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	ws, err := websocket.Upgrade(w, r)
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("webSocker Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
 	if err != nil {
 		fmt.Fprintf(w, "%+V\n", err)
 	}
-	go websocket.Writer(ws)
-	websocket.Reader(ws)
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+
 }
 
 func setupRoutes() {
-	http.HandleFunc("/ws", serveWs)
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
 }
 
 func main() {
